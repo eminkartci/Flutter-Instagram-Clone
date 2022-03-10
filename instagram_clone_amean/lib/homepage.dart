69f599d9d290f94a4670ba9661bddf76238cae24
eminkartci <emin.kartci@ozu.edu.tr>
2022-03-10 22:41:46 +0300

    Nav  Bar ile farkli sayfalar acabiliyoruz

--- instagram_clone_amean/lib/homepage.dart
+++ instagram_clone_amean/lib/homepage.dart
@@ -8,18 +8,38 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  // Define selected index
+  int _selectedIndex = 0;
+  void _navigateBottomNavBar(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  // Different Pages to navigate
+  final List<Widget> _subPages = [
+    Center(child: Text("HOME")),
+    Center(child: Text("SEARCH")),
+    Center(child: Text("REELS")),
+    Center(child: Text("SHOP")),
+    Center(child: Text("PROFILE"))
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Center(child: Text("Body Here !")),
+        body: _subPages[_selectedIndex],
         bottomNavigationBar: BottomNavigationBar(
+          currentIndex: _selectedIndex,
+          onTap: _navigateBottomNavBar,
           type: BottomNavigationBarType.fixed,
           items: [
             BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
-            BottomNavigationBarItem(icon: Icon(Icons.home), label: "Search"),
-            BottomNavigationBarItem(icon: Icon(Icons.home), label: "Reels"),
-            BottomNavigationBarItem(icon: Icon(Icons.home), label: "Shop"),
-            BottomNavigationBarItem(icon: Icon(Icons.home), label: "Profıle")
+            BottomNavigationBarItem(icon: Icon(Icons.search), label: "Search"),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.vibration), label: "Reels"),
+            BottomNavigationBarItem(icon: Icon(Icons.shop), label: "Shop"),
+            BottomNavigationBarItem(icon: Icon(Icons.person), label: "Profıle")
           ],
         ));
   }
