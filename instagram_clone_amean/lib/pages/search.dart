da5600c0ae7a89a61e0b95e0d5004689f09067df
eminkartci <emin.kartci@ozu.edu.tr>
2022-03-11 13:16:16 +0300

    Search index is done

--- instagram_clone_amean/lib/pages/search.dart
+++ instagram_clone_amean/lib/pages/search.dart
@@ -6,6 +6,34 @@ class SearchPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        title: ClipRRect(
+          borderRadius: BorderRadius.circular(8),
+          child: Container(
+            color: Colors.grey[200],
+            padding: EdgeInsets.all(10),
+            child: Row(
+              children: [
+                Icon(
+                  Icons.search,
+                  color: Colors.grey[500],
+                ),
+                SizedBox(
+                  width: 12,
+                ),
+                Container(
+                  child: Text(
+                    "Search",
+                    style: TextStyle(color: Colors.grey[500]),
+                  ),
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
       body: Center(child: Text("SEARCH")),
     );
   }
