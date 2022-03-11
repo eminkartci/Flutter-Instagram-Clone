1dd519fc28ea60e25691dbe5d8a2130b0f67ed62
eminkartci <emin.kartci@ozu.edu.tr>
2022-03-11 14:57:08 +0300

    Profil Sayfası Eklendi

--- instagram_clone_amean/lib/pages/profile.dart
+++ instagram_clone_amean/lib/pages/profile.dart
@@ -5,6 +5,104 @@ class ProfilePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Center(child: Text("PROFILE")));
+    return Scaffold(
+        body: Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Padding(
+          padding: const EdgeInsets.only(left: 20, top: 40.0),
+          child: Column(
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  // User PP
+                  Container(
+                      width: 100,
+                      height: 100,
+                      decoration: BoxDecoration(
+                          color: Colors.grey[300], shape: BoxShape.circle)),
+
+                  // Like Follower Folloe
+                  Expanded(
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        Column(
+                          children: const [
+                            Text(
+                              "1934",
+                              style: TextStyle(
+                                  fontWeight: FontWeight.bold, fontSize: 30),
+                            ),
+                            Text("Posts")
+                          ],
+                        ),
+                        Column(
+                          children: const [
+                            Text(
+                              "34",
+                              style: TextStyle(
+                                  fontWeight: FontWeight.bold, fontSize: 30),
+                            ),
+                            Text("Posts")
+                          ],
+                        ),
+                        Column(
+                          children: const [
+                            Text(
+                              "52",
+                              style: TextStyle(
+                                  fontWeight: FontWeight.bold, fontSize: 30),
+                            ),
+                            Text("Following")
+                          ],
+                        )
+                      ],
+                    ),
+                  )
+
+                  // Posts
+                ],
+              )
+            ],
+          ),
+        ),
+        Padding(
+          padding: EdgeInsets.all(20),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      "eminkartci",
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                    Row(
+                      children: [
+                        Icon(Icons.edit),
+                      ],
+                    )
+                  ],
+                ),
+              ),
+              const Padding(
+                padding: EdgeInsets.symmetric(vertical: 8.0),
+                child: Text(
+                    "Girisimci bir elektrik elektronik muhendisiyim. Bos zamanlarimda instagram clone yaparim."),
+              ),
+              const Text(
+                "www.eminkartci.com",
+                style: TextStyle(color: Colors.blue),
+              )
+            ],
+          ),
+        ),
+      ],
+    ));
   }
 }
