3c91f712a6cb2eae67ae8a704cb56320b5807e17
eminkartci <emin.kartci@ozu.edu.tr>
2022-03-11 13:27:23 +0300

    Shop Grid tamamlandı

--- instagram_clone_amean/lib/pages/shop.dart
+++ instagram_clone_amean/lib/pages/shop.dart
@@ -1,10 +1,42 @@
 import 'package:flutter/material.dart';
+import 'package:instagram_clone_amean/pages/util/explore_grid.dart';
+import 'package:instagram_clone_amean/pages/util/shop_grid.dart';
 
 class ShopPage extends StatelessWidget {
   const ShopPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Center(child: Text("SHOP")));
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        title: ClipRRect(
+          borderRadius: BorderRadius.circular(8),
+          child: Container(
+            color: Colors.grey[200],
+            padding: EdgeInsets.all(10),
+            child: Row(
+              children: [
+                Icon(
+                  Icons.search,
+                  color: Colors.grey[500],
+                ),
+                SizedBox(
+                  width: 12,
+                ),
+                Container(
+                  child: Text(
+                    "Search",
+                    style: TextStyle(color: Colors.grey[500]),
+                  ),
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
+      body: ShopGrid(),
+    );
   }
 }
